e9a5935351ff567917c97f68a776b446833bb6bb
Raymond Gauthier <jraygauthier@gmail.com>
2019-12-13 10:47:39 -0500

    Fix abs local link to relative support
    There was a hardcoded path in the code.

--- .build-system/pandoc-filters/local-links-abs-to-rel.lua
+++ .build-system/pandoc-filters/local-links-abs-to-rel.lua
@@ -7,7 +7,12 @@ local function is_local_link(uri)
   return nil == get_uri_scheme(uri)
 end
 
-local rel_path_to_site_root_dir = os.getenv("PANDOC_MD_WIKI_REL_PATH_FROM_PAGE_TO_ROOT_DIR")
+local function ensure_trailing_slash(str)
+  return string.gsub(str, "/$", "") .. "/"
+end
+
+local initial_slash_replacement_prefix = ensure_trailing_slash(
+  os.getenv("PANDOC_MD_WIKI_REL_PATH_FROM_PAGE_TO_ROOT_DIR"))
 
 function Link(el)
   if not is_local_link(el.target) then
@@ -16,6 +21,7 @@ function Link(el)
 
   -- print(string.format("site_root_dir: %s", rel_path_to_site_root_dir))
 
-  el.target = string.gsub(el.target, "^/", "../../")
+  el.target = string.gsub(
+    el.target, "^/", initial_slash_replacement_prefix)
   return el
 end
