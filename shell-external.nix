570343441872eb6136727ca7db7ca7b28852ce85
Raymond Gauthier <jraygauthier@gmail.com>
2021-08-18 12:47:44 -0400

    build(nix): add optional pdf support flag

--- shell-external.nix
+++ shell-external.nix
@@ -1,8 +1,10 @@
 { pkgs ? null
 , withVscodeSupport ? false
+, withPdfSupport ? false
 } @ args:
 
 (import ./release.nix { inherit pkgs; }).shell.mkExternal {
   inherit withVscodeSupport;
+  inherit withPdfSupport;
 }
 
--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 { pkgs ? null
 , withVscodeSupport ? false
+, withPdfSupport ? false
 } @ args:
 
 (import ./release.nix { inherit pkgs; }).shell.mkInternal {
   inherit withVscodeSupport;
+  inherit withPdfSupport;
 }
